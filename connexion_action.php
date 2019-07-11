d39e73ef9a46ddc64ac68b158af4f3c863ee8051
oro <olivier.razafindrabekoto@wealins.com>
2019-07-11 16:43:01 +0200

    Implement conexion 90% done

--- connexion_action.php
+++ connexion_action.php
@@ -5,34 +5,40 @@ include_once 'inc/db_connection_inc.php';
 
 
 //Déclaration des variables globales
-$utilisateurs = [];
-$_SESSION["login_empty"] = false;
+$utilisateur = [];
+$_SESSION["login_empty"] = null;
 
 // Recupération des données postées depuis l'écran.
-$username = $_POST["user_input"];
-$login = $_POST["password_input"];
+
+if(isset($_POST["username"]) && isset($_POST["password"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
 
-// Validation des données
-if(!isset($username) || !isset($login)) {
-    $_SESSION["login_empty"] = "Le nom ou mot de passe est obligatoire";
-    return;
-}
+    // Preparation de la requête
+    $query = "SELECT id_utilisateur, password, pseudo  FROM `utilisateurs` WHERE  (`email`='$username' OR `pseudo`='$username')";
 
-// Preparation de la requête
-$query = "SELECT email,password,pseudo  FROM `utilisateurs` WHERE `password`= " . password_hash('$login') ."  AND (`email`='$username' OR `pseudo`='$username')";
-
-$result = mysqli_query($con, $query) or die(mysqli_error($con));
+    $result = mysqli_query($con, $query) or die(mysqli_error($con));
 
 
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $utilisateurs[] = array("email" => $row["email"],
-            "password" => $row["password"],
-            "pseudo" => $row["pseudo"]
-        );
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+
+            if(password_verify($password, $row["password"])) {
+                $utilisateur = array("id_utilisateur" => $row["id_utilisateur"] , "pseudo" => $row["pseudo"] );
+                $_SESSION["user"] = $utilisateur;
+            }   else {
+                $_SESSION["error"] = "Le nom ou mot de passe est incorrect.";
+                break;
+            }
+        }
+    }
+
+    if(isset($_SESSION["user"])) {
+        header('Location: index.php');
+    } else {
+        header("Location: connexion.php");
     }
 }
 
-echo json_encode($utilisateurs, JSON_UNESCAPED_UNICODE);
 
